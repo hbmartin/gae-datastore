c7bb99e2e04403285b0328bfdcbe187a6907dd9c
Harold Martin <harold.martin@gmail.com>
2013-05-02 14:35:02 -0700

    fix for handling post

--- putHandler.py
+++ putHandler.py
@@ -12,20 +12,14 @@ from baseHandler import BaseRequestHandler
 class PutHandler(BaseRequestHandler):
 
   def post(self, key = None):
-    logging.error("post")
     record = None
     if not key:
       key = self.request.get('key')
-    # Is this an update ?
-    if key: 
+    if key:
       record = Record.get_by_key_name(key)
-      if not record:
-        record = Record(key_name = key, value = self.request.get('value'))
-      else:
-        record.value = self.request.get('value')
-    else: 
-      # Get a random key first
-      key = "a"+md5.new(str(random.random())).hexdigest()
+    if not record:
       record = Record(key_name = key, value = self.request.get('value'))
+    else:
+      record.value = self.request.get('value')
     record.put()
     self.response.out.write(key)
